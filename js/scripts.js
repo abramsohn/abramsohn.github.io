c0341f90c2415c85f86341a14da4f652498d9098
Ido Abramsohn <ido.abramsohn@gmail.com>
2021-04-17 15:27:41 -0400

    add script to standardize the vh measurement on mobile browsers

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
+// set custom css variable to 1% of visable screen
+// this will account for mobile browser notch
+const vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 const button = document.querySelector('button');
 const arrow = document.querySelector('.arrow');
  
 document.onload = animateArraw();
 button.addEventListener('click', handleClick);
- 
+
+
+// scroll to content when clicking the arrow on intro page
 function handleClick(e) {
     e.preventDefault();
     const main = document.querySelector('main');
@@ -15,7 +22,7 @@ function handleClick(e) {
   });
 }
 
-
+// intro page arrow animation
 function animateArraw() {
     setInterval(() =>
         setTimeout(() => {
